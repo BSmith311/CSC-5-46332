d1f829e58875f5ea3b140553a42aacf7d2256d7f
Brandon Smith <107975382+BSmith311@users.noreply.github.com>
2022-07-12 18:56:35 -0700

    Add files via upload

---
+++ Hmwk/HelloWorld/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Brandon Smith
+ * Created on June 23, 2022, 7:14 PM
+ * Purpose: Our First Program
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Mathematical/Physics/Conversions, Higher dimensioned arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map inputs to outputs -> The Process
+    
+    //Display Results
+    cout<<"Hello World"<<endl;
+    //Exit stage right
+        return 0;
+}
+
